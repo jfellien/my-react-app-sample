3cfe7c73449cb582f80c6916c56b16489c6f2797
Janek Fellien <jan.fellien@devcrowd.de>
2024-07-02 09:00:09 +0200

    refactor: Update Tickets component to use API data for rendering tickets

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,6 +1,35 @@
 import { Link } from "react-router-dom";
 
+const tickets = await( await fetch(`/api/tickets`)).json();
+
+const renderTicket = (ticket) => {
+    return (
+        <div key={crypto.randomUUID()}
+            className="bg-white border-2 border-kddd-light-blue p-8 md:w-auto lg:w-96 flex-shrink-0 rounded">
+            <p className="text-xl font-semibold text-kddd-light-blue">
+                {ticket.name}
+            </p>
+            <p className="text-kddd-dark-blue mt-2 mb-8">
+                Valid for {ticket.validFor}
+            </p>
+            <div className="w-full flex justify-center">
+                <span className="text-3xl font-bold text-kddd-dark-blue">
+                    € {ticket.price}
+                </span>
+            </div>
+            <Link to={ticket.bookingLink} target="_blank" >
+                <div
+                    className="w-full my-8 p-4 text-center font-bold rounded shadow-sm text-blue-900 bg-transparent border-2 border-blue-500 hover:bg-blue-50"
+                >
+                    Get your ticket here
+                </div>
+            </Link>
+        </div>
+    );
+}
+
 function Tickets() {
+
     return (
         <>
             <section id="tickets" className="bg-gray-300">
@@ -8,47 +37,7 @@ function Tickets() {
                     <div className="mx-auto max-w-4xl overflow-hidden lg:overflow-visible space-y-24">
                         <div className="w-full flex flex-col items-stretch lg:flex-row justify-around space-y-8 lg:space-y-0 lg:space-x-8">
 
-                            <div className="bg-white border-2 border-kddd-light-blue p-8 md:w-auto lg:w-96 flex-shrink-0 rounded">
-                                <p className="text-xl font-semibold text-kddd-light-blue">
-                                    One Day Ticket
-                                </p>
-                                <p className="text-kddd-dark-blue mt-2 mb-8">
-                                    Valid for one day
-                                </p>
-                                <div className="w-full flex justify-center">
-                                    <span className="text-3xl font-bold text-kddd-dark-blue">
-                                        € 100
-                                    </span>
-                                </div>
-                                <Link to="/home" target="_blank" >
-                                    <div
-                                        className="w-full my-8 p-4 text-center font-bold rounded shadow-sm text-blue-900 bg-transparent border-2 border-blue-500 hover:bg-blue-50"
-                                    >
-                                        Get your ticket here
-                                    </div>
-                                </Link>
-                            </div>
-
-                            <div className="bg-white border-2 border-kddd-light-blue p-8 md:w-auto lg:w-96 flex-shrink-0 rounded">
-                                <p className="text-xl font-semibold text-kddd-light-blue">
-                                    Two Days Ticket
-                                </p>
-                                <p className="text-kddd-dark-blue mt-2 mb-8">
-                                    Valid for two days
-                                </p>
-                                <div className="w-full flex justify-center">
-                                    <span className="text-3xl font-bold text-kddd-dark-blue">
-                                        € 200
-                                    </span>
-                                </div>
-                                <Link to="/home" target="_blank" >
-                                    <div
-                                        className="w-full my-8 p-4 text-center font-bold rounded shadow-sm text-blue-900 bg-transparent border-2 border-blue-500 hover:bg-blue-50"
-                                    >
-                                        Get your ticket here
-                                    </div>
-                                </Link>
-                            </div>
+                            {tickets.map(renderTicket)}
 
                         </div>
                     </div>
